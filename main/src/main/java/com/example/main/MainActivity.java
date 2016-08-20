d6f7457ac929477c27afa245099da3f0bcd86e39
melchauu <mel_o_di@hotmail.com>
2016-08-20 15:18:59 -0400

    Voice Recognition

--- main/src/main/java/com/example/main/MainActivity.java
+++ main/src/main/java/com/example/main/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.main;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -61,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        checkVoiceRecognition();
         initViews();
     }
 
@@ -79,7 +93,134 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             mGoogleApiClient.disconnect();
         }
     }
+    public String convToSpell (String phrase){
+        String lcPhrase = phrase.toLowerCase();
+        String spell = "";
+        String time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String payload = "";
+        if( (lcPhrase.matches("l.*mos")) || (lcPhrase.equals("loomis")) || (lcPhrase.matches(".*most"))  ) {
+            spell="lumos";
+        }
+        else if ((lcPhrase.matches("expel.*")) || (lcPhrase.matches("spell.*"))){
+            spell="expelliarmus";
+        }
+        else if ((lcPhrase.matches("stupid.*")) || (lcPhrase.matches("super.*"))){
+            spell="stupefy";
+        }
+        else if ((lcPhrase.matches("a.*c.*ra")) || (lcPhrase.matches("of.*c.*ra"))){
+            spell="avada kedavra";
+        }
+        else if ((lcPhrase.matches("conf.*go")) || (lcPhrase.matches("confirm.*go"))){
+            spell="confringo";
+        }
+        else if ((lcPhrase.matches("p.*t.*o")) || (lcPhrase.matches(".*ro.*t.*go"))){
+            spell="protego";
+        }
+        else if ((lcPhrase.matches("chris.*o")) || (lcPhrase.matches("chris.*y.*")) || (lcPhrase.matches("crew.*o"))){
+            spell="crucio";
+        }
+        else if (lcPhrase.matches(".*an.*send.*you")) {
+            spell="incendio";
+        }
+        else{
+            spell = lcPhrase;
+        }
+        payload = spell + "," + time;
+        return payload;
+    };
+    public void checkVoiceRecognition() {
+        // Check if voice recognition is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            mbtSpeak.setEnabled(false);
+            mbtSpeak.setText("Voice recognizer not present");
+            Toast.makeText(this, "Voice recognizer not present",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+                .getPackage().getName());
+
+        // Display an hint to the user about what he should say.
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, metTextHint.getText()
+                .toString());
+
+        // Given an hint to the recognizer about what the user is going to say
+        //There are two form of language model available
+        //1.LANGUAGE_MODEL_WEB_SEARCH : For short phrases
+        //2.LANGUAGE_MODEL_FREE_FORM  : If not sure about the words or phrases and its domain.
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+
+        // If number of Matches is not selected then return show toast message
+        if (msTextMatches.getSelectedItemPosition() == AdapterView.INVALID_POSITION) {
+            Toast.makeText(this, "Please select No. of Matches from spinner",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int noOfMatches = Integer.parseInt(msTextMatches.getSelectedItem()
+                .toString());
+        // Specify how many results you want to receive. The results will be
+        // sorted where the first result is the one with higher confidence.
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, noOfMatches);
+        //Start the Voice recognizer activity for the result.
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+            //If Voice recognition is successful then it returns RESULT_OK
+            if(resultCode == RESULT_OK) {
+
+                ArrayList<String> textMatchList = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                if (!textMatchList.isEmpty()) {
+                    // convert phrase to spells
+                    textMatchList.set(0, convToSpell (textMatchList.get(0)));
+                    // If first Match contains the 'search' word
+                    // Then start web search.
+                    if (textMatchList.get(0).contains("search")) {
+
+                        String searchQuery = textMatchList.get(0);
+                        searchQuery = searchQuery.replace("search","");
+                        Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+                        search.putExtra(SearchManager.QUERY, searchQuery);
+                        startActivity(search);
+                    } else {
+                        // populate the Matches
+                        mlvTextMatches
+                                .setAdapter(new ArrayAdapter<String>(this,
+                                        android.R.layout.simple_list_item_1,
+                                        textMatchList));
+                    }
+
+                }
+                //Result code for various error.
+            }else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+                showToastMessage("Audio Error");
+            }else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+                showToastMessage("Client Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+                showToastMessage("Network Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+                showToastMessage("No Match");
+            }else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+                showToastMessage("Server Error");
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    void showToastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
     private void initViews() {
         mStatusText = (TextView) findViewById( R.id.text_status );
         mConnectionButton = (Button) findViewById( R.id.button_connection );
